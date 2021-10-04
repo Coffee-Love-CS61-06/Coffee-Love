ade5d258cfffdb3f49a73759b362c35477d6b833
Sakdipat Ontoum <sakdipat3536@gmail.com>
2021-10-04 18:45:48 +0700

    .

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,5 @@
-// import 'dart:html';
-
 import 'package:coffee_love/predict.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,25 +7,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // File imageURI;
-  // String result;
-  // String path;
-  //
-  // Future getImageFromGallery() async {
-  //   var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-  //   setState(() {
-  //     imageURI = image as File;
-  //     path = image.path;
-  //   });
-  // }
-  //
-  // Future getImageFromCamera() async {
-  //   var image = await ImagePicker.pickImage(source: ImageSource.camera);
-  //   setState(() {
-  //     imageURI = image as File;
-  //     path = image.path;
-  //   });
-  // }
+
 
   @override
   Widget build(BuildContext context) {
@@ -47,57 +26,40 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  // margin: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(5),
                   width: MediaQuery.of(context).size.width,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Container(
-                          // margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                          margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
                           child: Text("Coffee Roast Intelligence")),
                       Container(
-                          // margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                          margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
                           child: IconButton(
-<<<<<<< Updated upstream
-                            onPressed: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Predict()),
-                              );
-                            },
-=======
                               onPressed: () {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(builder: (context) => Predict()),
                                 );
                               },
->>>>>>> Stashed changes
                             icon: Image.asset('assets/images/camera.png'),
                             iconSize: 350,
-                            // padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                            padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
                           )),
                       Container(
-                          // margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                           child: IconButton(
-<<<<<<< Updated upstream
-                            onPressed: (){
-=======
                             onPressed: () {
->>>>>>> Stashed changes
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(builder: (context) => Predict()),
                               );
                             },
-<<<<<<< Updated upstream
-                              icon: Image.asset('assets/images/photo.png'),
-=======
                             icon: Image.asset('assets/images/photo.png'),
->>>>>>> Stashed changes
                             iconSize: 350,
-                            // padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                            padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
                           )),
                     ],
                   ),
